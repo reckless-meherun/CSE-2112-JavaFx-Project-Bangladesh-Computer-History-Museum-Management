85199fb246ec876a45bfc85fe3cca16c38f20c42
himel-0r <himelchandraroy@gmail.com>
2023-04-11 04:47:54 +0600

    Curatorial Department Code done

---
+++ Code/Museum/src/main/java/Departments/Software.java
@@ -0,0 +1,18 @@
+package Departments;
+
+public class Software extends Curatorial_dept{
+    private String Documents;
+
+    public Software()
+    {
+
+    }
+    public void setAdditionDocuments (String doc)
+    {
+        this.Documents = doc;
+    }
+    public String getAdditionalDocuments ()
+    {
+        return this.Documents;
+    }
+}
