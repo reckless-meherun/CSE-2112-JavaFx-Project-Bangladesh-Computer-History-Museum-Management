de1c9a78926b8cac5afc00d8ec8e7f7bc526cefc
fatbomb <joarderaniket@gmail.com>
2023-04-11 03:00:55 +0600

    added class under people interface.

---
+++ Code/Museum/src/main/java/People/Gender.java
@@ -0,0 +1,5 @@
+package People;
+
+public enum Gender {
+    MALE,FEMALE,OTHER;
+}
---
+++ Code/Museum/src/main/java/People/people.java
@@ -0,0 +1,9 @@
+package People;
+
+public interface people {
+    public String Get_name();
+    public Gender Get_gender();
+    public String get_mobile_no();
+    public void set_mobile_no(String mobile_no);
+
+}
