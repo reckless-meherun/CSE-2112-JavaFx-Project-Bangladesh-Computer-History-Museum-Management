4a37197b224c1943666ea518e7d3ce49987e76a4
reckless-meherun <meherun2001@gmail.com>
2023-04-16 23:10:01 +0600

    images checking

--- Code/Museum/src/main/java/application/museum/HelloApplication.java
+++ Code/Museum/src/main/java/application/museum/HelloApplication.java
@@ -39,9 +39,3 @@ public class HelloApplication extends Application
         }
     }
 }
-
-//    FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//    Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//                stage.setScene(scene);
-//                stage.show();
