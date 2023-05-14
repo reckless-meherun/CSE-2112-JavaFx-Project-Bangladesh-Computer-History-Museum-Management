e976ef3f941d2b0815a8045e02d9346fc567b52c
fatbomb <joarderaniket@gmail.com>
2023-05-14 10:06:12 +0600

    project done

--- Code/Museum/src/main/java/application/museum/WelcomeToBCHM.java
+++ Code/Museum/src/main/java/application/museum/WelcomeToBCHM.java
@@ -4,12 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class WelcomeToBCHM extends Application
 {
+    StringBuilder getrespath()
+    {
+        StringBuilder resourcesPath = new StringBuilder(getClass().getResource("").getPath());
+        //int n=resourcesPath.length();
+        resourcesPath.deleteCharAt(0);
+        //System.out.println(resourcesPath);
+        for (int i = 0; i < resourcesPath.length(); i++)
+        {
+            if (resourcesPath.charAt(i) == '%')
+            {
+                resourcesPath.replace(i, i + 3, " ");
+            }
+            if (resourcesPath.charAt(i) == 't' && resourcesPath.charAt(i + 1) == 'a' && resourcesPath.charAt(i + 2) == 'r' && resourcesPath.charAt(i + 3) == 'g' && resourcesPath.charAt(i + 4) == 'e' && resourcesPath.charAt(i + 5) == 't' && resourcesPath.charAt(i + 6) == '/')
+            {
+                resourcesPath.delete(i - 1, resourcesPath.length());
+                break;
+            }
+        }
+        return resourcesPath;
+    }
     public static void main(String[] args)
     {
         launch();
@@ -25,6 +46,9 @@ public class WelcomeToBCHM extends Application
             String css = this.getClass().getResource("hello.css").toExternalForm();
             scene.getStylesheets().add(css);
             stage.setTitle("Bangladesh Computer History Museum Management System");
+            StringBuilder st=new StringBuilder(getrespath().append("\\src\\main\\resources\\Assets\\SmallLogo.png"));
+            Image img = new Image(st.toString());
+            stage.getIcons().add(img);
             //stage.setFullScreen(true);
             stage.setScene(scene);
             stage.setResizable(false);
